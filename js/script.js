cc62d4705f0719e8bff136e8754fd750f5be9db6
Mazen-023 <mazenmagdy337@gmail.com>
2024-10-04 02:17:56 +0300

    Files Formatting

--- js/script.js
+++ js/script.js
@@ -2,20 +2,20 @@ function toggleVideo() {
   const trailor = document.querySelector(".trailor");
   const video = document.querySelector("video");
   video.pause();
-  trailor.classList.toggle('active');
+  trailor.classList.toggle("active");
 }
 
 function changeBg(bg, title) {
   const banner = document.querySelector(".bannar");
   const contents = document.querySelectorAll(".content");
   banner.style.background = `url("../images/movies/${bg}")`;
-  banner.style.backgroundSize = 'cover';
-  banner.style.backgroundPosition = 'center';
+  banner.style.backgroundSize = "cover";
+  banner.style.backgroundPosition = "center";
 
-  contents.forEach(content => {
-    content.classList.remove('active');
-    if(content.classList.contains(title)){
-      content.classList.add('active');
+  contents.forEach((content) => {
+    content.classList.remove("active");
+    if (content.classList.contains(title)) {
+      content.classList.add("active");
     }
   });
 }
