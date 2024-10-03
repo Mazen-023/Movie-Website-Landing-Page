71af2ddb4fec20114b3c5e97ab058494497070fd
Mazen-023 <mazenmagdy337@gmail.com>
2024-10-04 02:15:36 +0300

    JavaScript coding
    Add toggle video function to allow opening and exist the video window, also add change background image function which allows the background color change if you navigate between films. Also, changed the size of the carousel section in the main.css file to a smaller one.

---
+++ js/script.js
@@ -0,0 +1,21 @@
+function toggleVideo() {
+  const trailor = document.querySelector(".trailor");
+  const video = document.querySelector("video");
+  video.pause();
+  trailor.classList.toggle('active');
+}
+
+function changeBg(bg, title) {
+  const banner = document.querySelector(".bannar");
+  const contents = document.querySelectorAll(".content");
+  banner.style.background = `url("../images/movies/${bg}")`;
+  banner.style.backgroundSize = 'cover';
+  banner.style.backgroundPosition = 'center';
+
+  contents.forEach(content => {
+    content.classList.remove('active');
+    if(content.classList.contains(title)){
+      content.classList.add('active');
+    }
+  });
+}
